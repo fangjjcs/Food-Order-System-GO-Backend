cbb6722080e3383c0bb2bfe2d9954b03f9dd7c6d
fangjjcs <grace963214789@gmail.com>
2022-02-24 21:01:18 +0800

    add .env and start.sh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Working-with-React-and-GO-backend
+
+### Run The Backend
+`./start.sh`
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -46,8 +46,10 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "Server Port To Listen On")
 	flag.StringVar(&cfg.env, "env", "development", "Application Environment (development|production)")
 	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://fang@localhost/afternoon-tea?sslmode=disable", "Postgres Connection String")
-	flag.StringVar(&cfg.jwt.secret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "secret")
+	// flag.StringVar(&cfg.jwt.secret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "secret")
 	flag.Parse()
+
+	cfg.jwt.secret = os.Getenv("GO_AFTERNOON_JWT")
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
